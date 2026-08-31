0c2017ee744116ca4f529d234f58178398b8c4f8
shivraj.dev <timilsenashivraj598@gmail.com>
2026-08-31 11:00:36 +0545

    Add functions for syncing and retrieving general reminder alarms

--- src/alarms.ts
+++ src/alarms.ts
@@ -1,4 +1,8 @@
-import { ALARM_NAME } from "./constants";
+import { ALARM_NAME, generalAlarmName } from "./constants";
+import {
+  nextOccurrence,
+  type GeneralReminder,
+} from "./general-reminders";
 import type { Settings } from "./settings";
 
 export async function syncAlarm(settings: Settings): Promise<void> {
@@ -15,4 +19,36 @@ export async function syncAlarm(settings: Settings): Promise<void> {
 export async function getNextAlarmTime(): Promise<number | undefined> {
   const alarm = await chrome.alarms.get(ALARM_NAME);
   return alarm?.scheduledTime;
+}
+
+export async function syncGeneralReminderAlarms(
+  reminders: GeneralReminder[],
+): Promise<void> {
+  const all = await chrome.alarms.getAll();
+  for (const alarm of all) {
+    if (alarm.name.startsWith("general-reminder-")) {
+      await chrome.alarms.clear(alarm.name);
+    }
+  }
+  for (const reminder of reminders) {
+    if (!reminder.enabled) {
+      continue;
+    }
+    await chrome.alarms.create(generalAlarmName(reminder.id), {
+      when: nextOccurrence(reminder.hour, reminder.minute),
+    });
+  }
+}
+
+export async function getNextGeneralAlarmTimes(): Promise<
+  Map<string, number>
+> {
+  const all = await chrome.alarms.getAll();
+  const map = new Map<string, number>();
+  for (const alarm of all) {
+    if (alarm.name.startsWith("general-reminder-")) {
+      map.set(alarm.name, alarm.scheduledTime);
+    }
+  }
+  return map;
 }
